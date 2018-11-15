0caacf8951603670cb1c8a8c7aaa61e63b7755ae
idochetrit <idochetrit.dev@gmail.com>
2018-11-16 01:00:12 +0200

    userScore class, bump to babel 7 and typescript

---
+++ src/users/user.score.ts
@@ -0,0 +1,26 @@
+class UserScore {
+  
+}
+// import _ from "lodash";
+
+// export default (() => {
+//   const WEIGHTS = {
+//     isStudent: 0.1
+//   };
+
+//   function normalizeFields({ isStudent }) {
+//     return { isStudent };
+//   }
+
+//   class UserScore {
+//     calculateScore(user) {
+//       const { isStudent } = user;
+//       const normals = normalizeFields({ isStudent });
+
+//       const isStudentScore = normals.isStudent * WEIGHTS.isStudent;
+//       return _.sum(isStudentScore);
+//     }
+//   }
+
+//   return new UserScore();
+// })();
