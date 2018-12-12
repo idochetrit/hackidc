b079b391447f3d190674287b45ef1a350d54e4f0
idochetrit <idochetrit.dev@gmail.com>
2018-12-12 18:42:26 +0200

    adds requiredEquipments field

--- src/migrations/20181031081336-create-user.js
+++ src/migrations/20181031081336-create-user.js
@@ -56,6 +56,9 @@ module.exports = {
         foodRestrictionType: {
           type: Sequelize.ARRAY(Sequelize.STRING)
         },
+        requiredEquipment: {
+          type: Sequelize.ARRAY(Sequelize.STRING)
+        },
         volunteerToAcceptLoner: {
           type: Sequelize.BOOLEAN
         },
